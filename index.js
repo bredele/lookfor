18b0caebb3f6e0cfa45b7fb01ed93b87b86bf4d9
bredele <olivier@petrofeed.com>
2013-09-21 11:54:36 -0600

    firs iteration, not tested

---
+++ index.js
@@ -0,0 +1,32 @@
+
+/**
+ * Expose 'search'
+ */
+
+module.exports = Search;
+
+
+/**
+ * Search constructor.
+ * @param {String} namespace
+ * @api public
+ */
+
+function Search(namespace){
+  this.namespace = namespace || "search";
+  this.style = document.createElement('style');
+  document.head.appendChild(this.style);
+}
+
+
+/**
+ * Run search.
+ * @param {String} value
+ * @param {string} className
+ * @api public
+ */
+
+Search.prototype.run = function(value, className) {
+  var searchable = className || 'searchable';
+  this.style.innerHTML = "." + searchable + ":not([data-search*=\'" + value + "']) { display: none; }";
+};
--- test/search.js
+++ test/search.js
@@ -2,6 +2,15 @@ var Search = require('search');
 var assert = require('assert');
 
 
-describe('', function(){
+// describe('Full text search', function(){
+//   it('should perform simple search', function(){
+//     var ul = document.createElement('ul');
+//     ul.innerHTML = "<li class='search' data-search='btnred'></li>" +
+//       "<li class='search' data-search='dropdownred'></li>" +
+//       "<li class='search' data-search='btngreen'></li>";
+//     document.body.appendChild(ul);
 
-});
+//     var search = new Search();
+//     search.run('btn');
+//   });
+// });
