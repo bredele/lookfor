a01f726233c79dc52061b60688681f68e886993b
bredele <olivier.wietrich@gmail.com>
2014-07-20 17:40:43 -0600

    add tests for single and multiple results

--- test/search.js
+++ test/search.js
@@ -1,22 +1,43 @@
 var lookfor = require('lookfor');
 var assert = require('assert');
 
+function  create(str, el) {
+	var li = document.createElement('li');
+	li.className = 'searchable';
+	li.setAttribute('data-search', str);
+	el.appendChild(li);
+	return li;
+}
 
 describe('Full text search', function(){
-	var list;
+	var list, red, blue, green;
 	beforeEach(function() {
 		list = document.createElement('ul');
 		document.body.appendChild(list);
-		list.innerHTML = "<li class='search' data-search='btnred'></li>" +
-      "<li class='search' data-search='dropdown'></li>" +
-      "<li class='search' data-search='btngreen'></li>";
-	});
-	
-  it('should perform simple search', function() {
-    var search = lookfor();
-    search.run('red');
 
-    assert.equal(list.childElementCount, 1);
-    assert.equal(list.firstChild.getAttribute('data-search'), 'btnred');
+		red = create('btnred', list);
+		blue = create('dropblue', list);
+		green = create('btngreen', list);
+	});
+
+	
+  it('should return multiple results', function() {
+    var search = lookfor();
+    search('btn');
+
+    assert.notEqual(window.getComputedStyle(red).display, 'none');
+    assert.equal(window.getComputedStyle(blue).display, 'none');
+    assert.notEqual(window.getComputedStyle(green).display, 'none');
   });
+	
+  it('should return a single result', function() {
+    var search = lookfor();
+    search('red');
+
+    assert.notEqual(window.getComputedStyle(red).display, 'none');
+    assert.equal(window.getComputedStyle(blue).display, 'none');
+    assert.equal(window.getComputedStyle(green).display, 'none');
+  });
+
+
 });
