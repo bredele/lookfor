4382eb8a80ff0406367097dbbefc540b32dc4d52
bredele <olivier.wietrich@gmail.com>
2014-07-20 17:28:28 -0600

    add tests

--- test/search.js
+++ test/search.js
@@ -1,16 +1,22 @@
-var Search = require('search');
+var lookfor = require('lookfor');
 var assert = require('assert');
 
 
-// describe('Full text search', function(){
-//   it('should perform simple search', function(){
-//     var ul = document.createElement('ul');
-//     ul.innerHTML = "<li class='search' data-search='btnred'></li>" +
-//       "<li class='search' data-search='dropdownred'></li>" +
-//       "<li class='search' data-search='btngreen'></li>";
-//     document.body.appendChild(ul);
+describe('Full text search', function(){
+	var list;
+	beforeEach(function() {
+		list = document.createElement('ul');
+		document.body.appendChild(list);
+		list.innerHTML = "<li class='search' data-search='btnred'></li>" +
+      "<li class='search' data-search='dropdown'></li>" +
+      "<li class='search' data-search='btngreen'></li>";
+	});
+	
+  it('should perform simple search', function() {
+    var search = lookfor();
+    search.run('red');
 
-//     var search = new Search();
-//     search.run('btn');
-//   });
-// });
+    assert.equal(list.childElementCount, 1);
+    assert.equal(list.firstChild.getAttribute('data-search'), 'btnred');
+  });
+});
